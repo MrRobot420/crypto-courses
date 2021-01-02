41b4bb3ff94f103dc2e8e283989fed71e1731536
MrRobot420 <max.karlchen@web.de>
2021-01-02 13:39:28 +0100

    add /src to tsconfig

--- src/routes/coinCap.ts
+++ src/routes/coinCap.ts
@@ -3,8 +3,9 @@ const coinCapRouter = express.Router()
 
 import { handleDataRetrieval } from '../controller/initialApiCall'
 
-coinCapRouter.get('/initial-listings', async () => {
+coinCapRouter.get('/initial-listings', async (req, res) => {
     await handleDataRetrieval()
+    return res.status(200).send()
 })
 
 export default coinCapRouter
