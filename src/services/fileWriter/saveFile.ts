e476dba57e43866dcb62b376837985212a0b6927
MrRobot420 <max.karlchen@web.de>
2021-01-02 13:29:34 +0100

    remove base_path | remove unused import

--- src/services/fileWriter/saveFile.ts
+++ src/services/fileWriter/saveFile.ts
@@ -1,7 +1,9 @@
 import fs from 'fs'
+import config from 'config'
 
 export const saveData = (data: object) => {
-    const path: string = '/Users/max/code/ts/crypto/apis/coin-market-cap/data/data.json'
+    const BASE_PATH: string = config.get('BASE_PATH')
+    const path: string = `${BASE_PATH}/data.json`
     console.log('writing data...')
     fs.writeFileSync(path, JSON.stringify(data))
 }
