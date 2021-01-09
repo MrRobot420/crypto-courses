86eeeb36452ee434dcf84648d8230332a74b6f80
MrRobot420 <max.karlchen@web.de>
2021-01-09 22:31:21 +0100

    small impro

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,30 @@
 import WebSocket from 'ws'
 
+const initWebSocketClient = (url: string): WebSocket => {
+    return new WebSocket(url, { perMessageDeflate: false })
+}
+
+const manageWebSocket = (ws: WebSocket, currency: string) => {
+    ws.on('open', function open() {
+        console.log(`opened ${currency} connection\n`)
+        ws.send(JSON.stringify({
+            "event": "bts:subscribe",
+            "data": {
+                "channel": `live_trades_${currency}`
+            }
+        }))
+    })
+    
+    ws.on('message', function incoming(receivedData) {
+        const tradeData: TradeData = JSON.parse(receivedData.toString())
+        const { data } = tradeData
+        
+        if (tradeData.event === 'trade') {
+            console.log(`${currency.split('eur')[0].toUpperCase()} Price: \t${data.price} €`)
+        }
+    })
+}
+
 type TradeData = {
     data: {
         buy_order_id: number,
@@ -15,32 +40,6 @@ type TradeData = {
     },
     event: string,
     channel: string
-}
-
-const initWebSocketClient = (url: string): WebSocket => {
-    return new WebSocket(url, { perMessageDeflate: false })
-}
-
-const manageWebSocket = (ws: WebSocket, currency: string) => {
-    ws.on('open', function open() {
-        console.log('opened connection')
-        ws.send(JSON.stringify({
-            "event": "bts:subscribe",
-            "data": {
-                "channel": `live_trades_${currency}`
-            }
-        }))
-    })
-    
-    ws.on('message', function incoming(receivedData) {
-        const tradeData: TradeData = JSON.parse(receivedData.toString())
-        const { data } = tradeData
-        
-        if (tradeData.event === 'trade') {
-            console.log(`Current ${currency.split('eur')[0].toUpperCase()} Price: \t${data.price} €`)
-            console.log()
-        }
-    })
 }
 
 export { 
