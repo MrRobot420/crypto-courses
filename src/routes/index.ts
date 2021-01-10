87460ed0bd089e0bd6abcbdf09df46f5a8c17832
MrRobot420 <max.karlchen@web.de>
2021-01-10 16:29:43 +0100

    add route for priceInfo of coinbase (no return value possible unfortunately)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 const routes = express.Router()
 
 import coinCapRouter from './coinCap'
+import coinbaseRouter from './coinbase'
 import ethereumRouter from './ethereum'
 import bitcoinRouter from './bitcoin'
 import litecoinRouter from './litecoin'
 
 routes.use(coinCapRouter)
+routes.use(coinbaseRouter)
 routes.use(ethereumRouter)
 routes.use(bitcoinRouter)
 routes.use(litecoinRouter)
--- src/services/apiConnector/coinbase/market.ts
+++ src/services/apiConnector/coinbase/market.ts
@@ -1,13 +1,14 @@
 import initClient from './init'
 
-const getBuyPrice = (currencyPair: string): object => {
-    const client = initClient()
-    let data = {}
-    client.getBuyPrice({ 'currencyPair': currencyPair }, (err, obj) => {
-        console.log(`total amount to buy 1 ${currencyPair}: ${obj.data.amount}`)
-        data = obj.data
+const getBuyPrice = async (currencyPair: string) => {
+    const client = await initClient()
+    await client.getBuyPrice({ 'currencyPair': currencyPair }, (err, obj) => {
+        if (!err) {
+            console.log(`total amount to buy 1 ${currencyPair}: ${obj.data.amount}`)
+        } else {
+            console.log('ERROR! ', err.message)
+        }
     })
-    return data
 } 
 
 export {
