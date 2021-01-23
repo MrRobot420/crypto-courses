9d1bb585003d63e2d555de3ae2a4cc3b0201ae58
MrRobot420 <max.karlchen@web.de>
2021-01-23 08:43:06 +0100

    finish module exports for storage

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -1,5 +1,15 @@
 import Connection from './connection'
+import { bitcoinModel } from './models/bitcoin'
+import { bitcoincashModel } from './models/bitcoincash'
+import { ethereumModel } from './models/ethereum'
+import { litecoinModel } from './models/litecoin'
+import { rippleModel } from './models/ripple'
 
 export {
-    Connection
+    Connection,
+    bitcoinModel,
+    bitcoincashModel,
+    ethereumModel,
+    litecoinModel,
+    rippleModel
 }
