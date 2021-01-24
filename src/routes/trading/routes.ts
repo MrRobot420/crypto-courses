05b6a0954c8154863fce61d165c5cf93ebab9d00
MrRobot420 <max.karlchen@web.de>
2021-01-24 08:02:19 +0100

    add more information and functionality to /buy

--- src/routes/trading/routes.ts
+++ src/routes/trading/routes.ts
@@ -1,21 +1,26 @@
 import express from 'express'
 const tradingRouter = express.Router()
 
-import { getCourseData } from '../../storage/operations'
+import { getCourseData, saveTransaction } from '../../storage/operations'
 
 tradingRouter.post('/buy', async (req, res): Promise<object> => {
-    const { currency } = req.body
+    const { currency, amount, userId } = req.body
 
-    if (currency) {
+    if (currency && userId && amount) {
         try {
             const data = await getCourseData(currency)
             const totalEntries = data!.courses.length - 1
             const { date, price, marketplace } = data?.courses[totalEntries]
             const latestCourseData = { date, price, marketplace }
-            const confirmationMessage = `TRADE: BOUGHT ${currency} for a price of ${price}€ ...`
+            const sum = (amount * price).toFixed(2)
+            const confirmationMessage = `${userId} BOUGHT ${amount} ${currency} at a price of ${price}€. - SUM: ${sum}€`
             console.log(confirmationMessage)
             
             const confirmationData = {
+                currency,
+                amount,
+                userId,
+                sum,
                 latestCourseData,
                 message: confirmationMessage
             }
@@ -25,7 +30,7 @@ tradingRouter.post('/buy', async (req, res): Promise<object> => {
             return res.status(500).send({ error: `ERROR while trying to buy ${currency}.` })        
         }
     }
-    return res.status(404).send({ error: `Please provide a currency. Retrieved: ${currency}` })
+    return res.status(404).send({ error: `ERROR. Need to provide currency, amount and userId. Retrieved: ${currency}, ${userId}` })
 })
 
 export default tradingRouter
--- src/storage/operations.ts
+++ src/storage/operations.ts
@@ -1,21 +1,6 @@
 import { cryptoModel } from './index'
 import ICryptoDoc from './models/cryptoDoc'
 
-
-const saveCourse = async (currency: string, price: number, time: string) => {
-    const course = { price, date: time, marketplace: 'bitstamp' }
-    const currentData: ICryptoDoc = (await cryptoModel.find({ currency }))[0]
-
-    if (currentData) {
-        const previousCourses = currentData.courses
-        previousCourses.push(course)
-        const response = await cryptoModel.findOneAndUpdate({ currency }, { courses: previousCourses }, { useFindAndModify: false })
-        if (response !== null) console.log(`Updated ${currency} course data.`);
-    } else {
-        await cryptoModel.create({currency, courses: [course]})
-    }
-}
-
 const getCourseData = async (currency: string): Promise<ICryptoDoc> => {
     try {
         const response = await cryptoModel.findOne({ currency })
@@ -26,7 +11,26 @@ const getCourseData = async (currency: string): Promise<ICryptoDoc> => {
     return await cryptoModel.find({ currency })
 }
 
+const saveCourse = async (currency: string, price: number, time: string) => {
+    const course = { price, date: time, marketplace: 'bitstamp' }
+    const currentData: ICryptoDoc = (await cryptoModel.find({ currency }))[0]
+
+    if (currentData) {
+        const previousCourses = currentData.courses
+        previousCourses.push(course)
+        const response = await cryptoModel.findOneAndUpdate({ currency }, { courses: previousCourses }, { useFindAndModify: false })
+        if (response !== null) console.log(`Updated ${currency} course data.\n`);
+    } else {
+        await cryptoModel.create({currency, courses: [course]})
+    }
+}
+
+const saveTransaction = async (currency: string, price: number, amount: number, userId: string, time: string) => {
+    
+}
+
 export {
+    getCourseData,
     saveCourse,
-    getCourseData
+    saveTransaction
 }
